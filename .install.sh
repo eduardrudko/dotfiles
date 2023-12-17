174237c828e367eff5fb838fd8477dae3de6dc4a
Eduard Rudko <erudko@siftscience.com>
2023-12-17 21:37:14 +0200

    update to install.sh

--- .install.sh
+++ .install.sh
@@ -1,38 +1,48 @@
 #!/usr/bin/env zsh
-main() {
-  if [ -z "${ZSH_VERSION:-}" ]; then
-    abort "ZShell is required in order to run this script"
-  fi
-
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    if ! is_runnable brew; then
-      abort "Homebrew is required in order to run this script"
-    fi
-  fi
-  
-  if ! is_runnable git; then
-    abort "Git is required in order to run this script"
-  fi
-  
-  SCRIPTPATH="${0:a:h}"
-  
-  echo "== Updating git submodules =="
-  
-  config -C "$SCRIPTPATH" submodule update --init
-}
-
-
 abort() {
   printf "%s\n" "$@"
   exit 1
 }
 
+if [ -z "${ZSH_VERSION:-}" ]; then
+  abort "ZShell is required in order to run this script"
+fi
+
 is_runnable() {
   command -v "$@" >/dev/null 2>&1
 }
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if ! is_runnable brew; then
+    abort "Homebrew is required in order to run this script"
+  fi
+fi
+
+if ! is_runnable git; then
+  abort "Git is required in order to run this script"
+fi
+
+SCRIPTPATH="${0:a:h}"
 
 config() {
   git --git-dir="$HOME/.cfg/" --work-tree="$HOME" "$@"
 }
 
-main "%@"; exit
+echo "== Updating git submodules =="
+config -C "$SCRIPTPATH" submodule update --init
+
+echo "== Updating config's config =="
+config config --local status.showUntrackedFiles no
+
+if [ -z "$SKIP_PACKAGES" ]; then
+  echo "== Installing packages =="
+
+  if is_runnable brew; then
+    brew install "$(cat "$SCRIPTPATH"/.homebrew_packages)"
+  elif is_runnable apt-get; then
+    sudo apt-get install -y "$(cat "$SCRIPTPATH"/apt-get_packages)"
+  else
+    abort "Don't know how to install packages on this platform. Aborting..."
+  fi
+fi
+
--- .test.dockerfile
+++ .test.dockerfile
@@ -5,7 +5,6 @@ RUN apt-get update && \
   apt-get install -y \
   git \
   sudo \
-  vim \
   zsh
 
 RUN git clone --bare https://github.com/eduardrudko/dotfiles.git $HOME/.cfg
@@ -13,7 +12,8 @@ RUN git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 RUN rm $HOME/.install.sh
 COPY .install.sh /root/.install.sh
 RUN chmod +x $HOME/.install.sh
+WORKDIR /root
+
 RUN $HOME/.install.sh
 
-WORKDIR /root
 ENTRYPOINT zsh
