be991a43aacb568fe58c4d19bbe827320b03cb46
Eduard Rudko <124259501+erudko-sift@users.noreply.github.com>
2023-12-28 16:59:50 +0200

    Add vim-jellybeans submodule (#2)

--- .modules/01-oh-my-zsh/install.sh
+++ .modules/01-oh-my-zsh/install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/zsh
-#
-echo "=== Installling oh-my-zsh ==="
-
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
+
 mv "$HOME/.zshrc.pre-oh-my-zsh" "$HOME/.zshrc"
