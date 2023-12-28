987a063e47bb4794659d9911b0f370c068d36e87
Eduard Rudko <erudko@siftscience.com>
2023-12-28 23:13:10 +0200

    create temp assets for zsh install

--- .modules/01-oh-my-zsh/install.sh
+++ .modules/01-oh-my-zsh/install.sh
@@ -2,3 +2,5 @@
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 
 mv "$HOME/.zshrc.pre-oh-my-zsh" "$HOME/.zshrc"
+
+touch .zsh/assets/.zshrc_t_env .zsh/assets/.zshrc_t_aliases
