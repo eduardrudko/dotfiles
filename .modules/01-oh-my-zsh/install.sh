41b25d2b2ee87dcdf5d67861189420e97c4357c0
Eduard Rudko <erudko@siftscience.com>
2023-12-29 14:21:07 +0200

    update oh my zsh module install script

--- .modules/01-oh-my-zsh/install.sh
+++ .modules/01-oh-my-zsh/install.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 
-mv "$HOME/.zshrc.pre-oh-my-zsh" "$HOME/.zshrc"
+zshrc="$HOME/.zshrc"
+original_zshrc="$HOME/.zshrc.pre-oh-my-zsh"
+zshrc_assets=(".zsh/assets/.zshrc_t_env" ".zsh/assets/.zshrc_t_aliases")
 
-touch .zsh/assets/.zshrc_t_env .zsh/assets/.zshrc_t_aliases
+if [ -e "$original_zshrc" ]; then
+  mv "$original_zshrc" "$zshrc"
+  echo "Updated .zshrc"
+fi
+
+for zshrc_asset in "${zshrc_assets[@]}"; do
+  if [ ! -e "$zshrc_asset" ]; then
+    touch "$zshrc_asset"
+    echo "Created $zshrc_asset"
+  fi
+done
